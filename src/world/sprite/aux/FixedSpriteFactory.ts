e704e8df749eaf40cf9de519ac62935492b48b40
Jack Le Hamster <jacklehamster@gmail.com>
2024-02-26 23:55:56 -0800

    WIP

--- src/world/sprite/aux/FixedSpriteFactory.ts
+++ src/world/sprite/aux/FixedSpriteFactory.ts
@@ -1,4 +1,3 @@
-import { CellUtils } from "utils/cell-utils";
 import { Sprite } from "../Sprite";
 import { IElemFactory } from "./IElemFactory";
 import { PositionUtils } from "dok-matrix";
@@ -8,27 +7,22 @@ import { copySprite } from "../utils/sprite-utils";
 import { Auxiliary } from "world/aux/Auxiliary";
 import { AuxiliaryHolder } from "world/aux/AuxiliaryHolder";
 import { Vector } from "dok-types";
+import { cellTag } from "cell-tracker";
 
 interface Config {
   cellSize?: number;
   spriteLimit?: number;
 }
 
-interface Props {
-  cellUtils: CellUtils;
-}
-
 export class FixedSpriteFactory extends AuxiliaryHolder implements IElemFactory<Sprite>, Auxiliary {
   private readonly spritesPerCell: Map<Tag, Sprite[]> = new Map();
   private readonly spritesList: List<Sprite>[];
-  private readonly cellUtils: CellUtils;
   readonly #tempVector: Vector = [0, 0, 0];
   private readonly config: Config;
 
-  constructor({ cellUtils }: Props, config: Config, ...spritesList: (List<Sprite> & Partial<Auxiliary>)[]) {
+  constructor(config: Config, ...spritesList: (List<Sprite> & Partial<Auxiliary>)[]) {
     super();
     this.spritesList = spritesList;
-    this.cellUtils = cellUtils;
     this.config = config;
     spritesList.forEach(sprites => this.addAuxiliary(sprites));
   }
@@ -39,11 +33,11 @@ export class FixedSpriteFactory extends AuxiliaryHolder implements IElemFactory<
       forEach(sprites, sprite => {
         if (sprite) {
           PositionUtils.transformToPosition(sprite.transform, this.#tempVector);
-          const cell = this.cellUtils.cellFromPos(this.#tempVector, this.config.cellSize ?? 1);
-          if (!this.spritesPerCell.has(cell.tag)) {
-            this.spritesPerCell.set(cell.tag, []);
+          const tag = cellTag(this.#tempVector[0], this.#tempVector[1], this.#tempVector[2], this.config.cellSize ?? 1);
+          if (!this.spritesPerCell.has(tag)) {
+            this.spritesPerCell.set(tag, []);
           }
-          this.spritesPerCell.get(cell.tag)?.push(copySprite(sprite));
+          this.spritesPerCell.get(tag)?.push(copySprite(sprite));
         }
       });
     });
